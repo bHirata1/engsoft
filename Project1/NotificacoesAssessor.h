ba2e4f6a463b473ac201137bb50fc477021af34e
David <deidomatos@gmail.com>
2018-05-09 09:22:23 -0300

    Adicionadas as 3 telas de assessor.

---
+++ Project1/NotificacoesAssessor.h
@@ -0,0 +1,449 @@
+#pragma once
+
+namespace Project1 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for NotificacoesAssessor
+	/// </summary>
+	public ref class NotificacoesAssessor : public System::Windows::Forms::Form
+	{
+	public:
+		NotificacoesAssessor(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~NotificacoesAssessor()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^  label2;
+	protected:
+	private: System::Windows::Forms::LinkLabel^  linkLabel3;
+	private: System::Windows::Forms::LinkLabel^  linkLabel2;
+	private: System::Windows::Forms::LinkLabel^  linkLabel1;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label6;
+	private: System::Windows::Forms::Button^  button6;
+	private: System::Windows::Forms::Button^  button4;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Button^  button5;
+	private: System::Windows::Forms::Button^  button3;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::PictureBox^  pictureBox3;
+	private: System::Windows::Forms::PictureBox^  pictureBox2;
+	private: System::Windows::Forms::PictureBox^  pictureBox1;
+	private: System::Windows::Forms::DataGridView^  dataGridView1;
+	private: System::Windows::Forms::ComboBox^  comboBox1;
+	private: System::Windows::Forms::DateTimePicker^  dateTimePicker2;
+	private: System::Windows::Forms::DateTimePicker^  dateTimePicker1;
+	private: System::Windows::Forms::Label^  label4;
+	private: System::Windows::Forms::Label^  label5;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::Label^  label7;
+	private: System::Windows::Forms::Label^  label8;
+	private: System::Windows::Forms::Button^  button7;
+	private: System::Windows::Forms::Button^  button8;
+	private: System::Windows::Forms::Button^  button9;
+	private: System::Windows::Forms::Label^  label9;
+	private: System::Windows::Forms::Label^  label10;
+	private: System::Windows::Forms::Label^  label11;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->linkLabel3 = (gcnew System::Windows::Forms::LinkLabel());
+			this->linkLabel2 = (gcnew System::Windows::Forms::LinkLabel());
+			this->linkLabel1 = (gcnew System::Windows::Forms::LinkLabel());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->button6 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->pictureBox3 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->dateTimePicker2 = (gcnew System::Windows::Forms::DateTimePicker());
+			this->dateTimePicker1 = (gcnew System::Windows::Forms::DateTimePicker());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->label8 = (gcnew System::Windows::Forms::Label());
+			this->button7 = (gcnew System::Windows::Forms::Button());
+			this->button8 = (gcnew System::Windows::Forms::Button());
+			this->button9 = (gcnew System::Windows::Forms::Button());
+			this->label9 = (gcnew System::Windows::Forms::Label());
+			this->label10 = (gcnew System::Windows::Forms::Label());
+			this->label11 = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F));
+			this->label2->Location = System::Drawing::Point(14, 99);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(297, 31);
+			this->label2->TabIndex = 9;
+			this->label2->Text = L"> Lista de Notificações:";
+			// 
+			// linkLabel3
+			// 
+			this->linkLabel3->AutoSize = true;
+			this->linkLabel3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F));
+			this->linkLabel3->Location = System::Drawing::Point(626, 25);
+			this->linkLabel3->Name = L"linkLabel3";
+			this->linkLabel3->Size = System::Drawing::Size(150, 24);
+			this->linkLabel3->TabIndex = 8;
+			this->linkLabel3->TabStop = true;
+			this->linkLabel3->Text = L"NOTIFICAÇÕES";
+			// 
+			// linkLabel2
+			// 
+			this->linkLabel2->AutoSize = true;
+			this->linkLabel2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F));
+			this->linkLabel2->Location = System::Drawing::Point(432, 25);
+			this->linkLabel2->Name = L"linkLabel2";
+			this->linkLabel2->Size = System::Drawing::Size(188, 24);
+			this->linkLabel2->TabIndex = 7;
+			this->linkLabel2->TabStop = true;
+			this->linkLabel2->Text = L"LISTA DE BURACOS";
+			// 
+			// linkLabel1
+			// 
+			this->linkLabel1->AutoSize = true;
+			this->linkLabel1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F));
+			this->linkLabel1->Location = System::Drawing::Point(312, 25);
+			this->linkLabel1->Name = L"linkLabel1";
+			this->linkLabel1->Size = System::Drawing::Size(114, 24);
+			this->linkLabel1->TabIndex = 6;
+			this->linkLabel1->TabStop = true;
+			this->linkLabel1->Text = L"CADASTRO";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 28.25F));
+			this->label1->Location = System::Drawing::Point(12, 9);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(281, 44);
+			this->label1->TabIndex = 5;
+			this->label1->Text = L"INFO BURACO";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Location = System::Drawing::Point(171, 287);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(61, 13);
+			this->label6->TabIndex = 29;
+			this->label6->Text = L"Notificação";
+			// 
+			// button6
+			// 
+			this->button6->Location = System::Drawing::Point(652, 510);
+			this->button6->Name = L"button6";
+			this->button6->Size = System::Drawing::Size(75, 23);
+			this->button6->TabIndex = 28;
+			this->button6->Text = L"Excluir";
+			this->button6->UseVisualStyleBackColor = true;
+			this->button6->Click += gcnew System::EventHandler(this, &NotificacoesAssessor::button6_Click);
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(652, 398);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(75, 23);
+			this->button4->TabIndex = 27;
+			this->button4->Text = L"Excluir";
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(652, 300);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 26;
+			this->button2->Text = L"Excluir";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this->button5->Location = System::Drawing::Point(559, 511);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(75, 23);
+			this->button5->TabIndex = 24;
+			this->button5->Text = L"Editar";
+			this->button5->UseVisualStyleBackColor = true;
+			this->button5->Click += gcnew System::EventHandler(this, &NotificacoesAssessor::button5_Click);
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(559, 398);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(75, 23);
+			this->button3->TabIndex = 23;
+			this->button3->Text = L"Editar";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(559, 301);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 25;
+			this->button1->Text = L"Editar";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// pictureBox3
+			// 
+			this->pictureBox3->Location = System::Drawing::Point(49, 482);
+			this->pictureBox3->Name = L"pictureBox3";
+			this->pictureBox3->Size = System::Drawing::Size(100, 70);
+			this->pictureBox3->TabIndex = 22;
+			this->pictureBox3->TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this->pictureBox2->Location = System::Drawing::Point(49, 376);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(100, 70);
+			this->pictureBox2->TabIndex = 21;
+			this->pictureBox2->TabStop = false;
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Location = System::Drawing::Point(49, 274);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(100, 70);
+			this->pictureBox1->TabIndex = 20;
+			this->pictureBox1->TabStop = false;
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Location = System::Drawing::Point(20, 246);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->Size = System::Drawing::Size(745, 324);
+			this->dataGridView1->TabIndex = 19;
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->DisplayMember = L"Todos";
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(4) { L"Todos", L"Finalizado", L"Em andamento", L"Não iniciado" });
+			this->comboBox1->Location = System::Drawing::Point(74, 203);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(121, 21);
+			this->comboBox1->TabIndex = 18;
+			// 
+			// dateTimePicker2
+			// 
+			this->dateTimePicker2->Location = System::Drawing::Point(353, 161);
+			this->dateTimePicker2->Name = L"dateTimePicker2";
+			this->dateTimePicker2->Size = System::Drawing::Size(209, 20);
+			this->dateTimePicker2->TabIndex = 17;
+			// 
+			// dateTimePicker1
+			// 
+			this->dateTimePicker1->Location = System::Drawing::Point(100, 161);
+			this->dateTimePicker1->Name = L"dateTimePicker1";
+			this->dateTimePicker1->Size = System::Drawing::Size(209, 20);
+			this->dateTimePicker1->TabIndex = 16;
+			this->dateTimePicker1->Value = System::DateTime(2018, 4, 1, 0, 0, 0, 0);
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.25F));
+			this->label4->Location = System::Drawing::Point(315, 164);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(32, 17);
+			this->label4->TabIndex = 14;
+			this->label4->Text = L"até:";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.25F));
+			this->label5->Location = System::Drawing::Point(16, 203);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(52, 17);
+			this->label5->TabIndex = 15;
+			this->label5->Text = L"Status:";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.25F));
+			this->label3->Location = System::Drawing::Point(16, 161);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(81, 17);
+			this->label3->TabIndex = 13;
+			this->label3->Text = L"Período de:";
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Location = System::Drawing::Point(171, 386);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(61, 13);
+			this->label7->TabIndex = 29;
+			this->label7->Text = L"Notificação";
+			// 
+			// label8
+			// 
+			this->label8->AutoSize = true;
+			this->label8->Location = System::Drawing::Point(171, 497);
+			this->label8->Name = L"label8";
+			this->label8->Size = System::Drawing::Size(61, 13);
+			this->label8->TabIndex = 29;
+			this->label8->Text = L"Notificação";
+			// 
+			// button7
+			// 
+			this->button7->Location = System::Drawing::Point(466, 301);
+			this->button7->Name = L"button7";
+			this->button7->Size = System::Drawing::Size(75, 23);
+			this->button7->TabIndex = 30;
+			this->button7->Text = L"Visualizar";
+			this->button7->UseVisualStyleBackColor = true;
+			// 
+			// button8
+			// 
+			this->button8->Location = System::Drawing::Point(466, 398);
+			this->button8->Name = L"button8";
+			this->button8->Size = System::Drawing::Size(75, 23);
+			this->button8->TabIndex = 30;
+			this->button8->Text = L"Visualizar";
+			this->button8->UseVisualStyleBackColor = true;
+			// 
+			// button9
+			// 
+			this->button9->Location = System::Drawing::Point(466, 511);
+			this->button9->Name = L"button9";
+			this->button9->Size = System::Drawing::Size(75, 23);
+			this->button9->TabIndex = 30;
+			this->button9->Text = L"Visualizar";
+			this->button9->UseVisualStyleBackColor = true;
+			this->button9->Click += gcnew System::EventHandler(this, &NotificacoesAssessor::button9_Click);
+			// 
+			// label9
+			// 
+			this->label9->AutoSize = true;
+			this->label9->Location = System::Drawing::Point(171, 311);
+			this->label9->Name = L"label9";
+			this->label9->Size = System::Drawing::Size(70, 13);
+			this->label9->TabIndex = 31;
+			this->label9->Text = L"Data: --/--/----";
+			// 
+			// label10
+			// 
+			this->label10->AutoSize = true;
+			this->label10->Location = System::Drawing::Point(171, 408);
+			this->label10->Name = L"label10";
+			this->label10->Size = System::Drawing::Size(70, 13);
+			this->label10->TabIndex = 32;
+			this->label10->Text = L"Data: --/--/----";
+			// 
+			// label11
+			// 
+			this->label11->AutoSize = true;
+			this->label11->Location = System::Drawing::Point(171, 521);
+			this->label11->Name = L"label11";
+			this->label11->Size = System::Drawing::Size(70, 13);
+			this->label11->TabIndex = 32;
+			this->label11->Text = L"Data: --/--/----";
+			// 
+			// NotificacoesAssessor
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(787, 592);
+			this->Controls->Add(this->label11);
+			this->Controls->Add(this->label10);
+			this->Controls->Add(this->label9);
+			this->Controls->Add(this->button9);
+			this->Controls->Add(this->button8);
+			this->Controls->Add(this->button7);
+			this->Controls->Add(this->label8);
+			this->Controls->Add(this->label7);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->button6);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button5);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->pictureBox3);
+			this->Controls->Add(this->pictureBox2);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->dataGridView1);
+			this->Controls->Add(this->comboBox1);
+			this->Controls->Add(this->dateTimePicker2);
+			this->Controls->Add(this->dateTimePicker1);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->linkLabel3);
+			this->Controls->Add(this->linkLabel2);
+			this->Controls->Add(this->linkLabel1);
+			this->Controls->Add(this->label1);
+			this->Name = L"NotificacoesAssessor";
+			this->Text = L"NotificacoesAssessor";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button9_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+private: System::Void button5_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+private: System::Void button6_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+};
+}
