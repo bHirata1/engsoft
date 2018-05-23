26d49faa51965e49788d8d47d75b4c3ebe12b329
n3wrose <9349388@W31>
2018-05-23 11:44:44 -0300

    Criacao Ordem Servico h, cpp

--- Project1/Ordem_de_Servico.cpp
+++ Project1/Ordem_de_Servico.cpp
@@ -1,188 +1,71 @@
 #include "Ordem_de_Servico.h"
-//Begin section for file Ordem_de_Servico.cpp
-//TODO: Add definitions that you want preserved
-//End section for file Ordem_de_Servico.cpp
 
+Ordem_de_Servico::Ordem_de_Servico()
+{
 
+}
 
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-Ordem_de_Servico::Ordem_de_Servico() 
+Ordem_de_Servico::~Ordem_de_Servico()
 {
-    //TODO Auto-generated method stub
+
 }
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-Ordem_de_Servico::~Ordem_de_Servico() 
+
+int Ordem_de_Servico::getidordem()
 {
-    //TODO Auto-generated method stub
+	return this->idordem;
 }
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-int Ordem_de_Servico::get_id() 
+
+void Ordem_de_Servico::setidordem(int id)
 {
-    //TODO Auto-generated method stub
-    return id;
+	this->idordem = id;
 }
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-void Ordem_de_Servico::set_id(int id) 
+
+int Ordem_de_Servico::gethorapessoal()
 {
-    //TODO Auto-generated method stub
-    this->id = id;
+	return this->horapessoal;
 }
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-time Ordem_de_Servico::get_horaPessoal() 
+
+void Ordem_de_Servico::sethorapessoal(int hora)
 {
-    //TODO Auto-generated method stub
-    return horaPessoal;
+	this->horapessoal = hora;
 }
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-void Ordem_de_Servico::set_horaPessoal(time horaPessoal) 
+
+time_t Ordem_de_Servico::getdata();
 {
-    //TODO Auto-generated method stub
-    this->horaPessoal = horaPessoal;
+	return this->data;
 }
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-Material Ordem_de_Servico::get_material() 
+
+void Ordem_de_Servico::setdata(time_t data)
 {
-    //TODO Auto-generated method stub
-    return material;
+	this->data = data;
 }
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-void Ordem_de_Servico::set_material(Material material) 
+
+int Ordem_de_Servico::getprioridade()
 {
-    //TODO Auto-generated method stub
-    this->material = material;
+	return this->prioridade;
 }
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-Time Ordem_de_Servico::get_horaEquipamento() 
+
+void Ordem_de_Servico::setprioridade(int prioridade)
 {
-    //TODO Auto-generated method stub
-    return horaEquipamento;
+	this->prioridade = prioridade;
 }
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-void Ordem_de_Servico::set_horaEquipamento(Time horaEquipamento) 
+
+string Ordem_de_Servico::getstatus()
 {
-    //TODO Auto-generated method stub
-    this->horaEquipamento = horaEquipamento;
+	return this->status;
 }
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-string Ordem_de_Servico::get_data() 
+
+void Ordem_de_Servico::setstatus(string status)
 {
-    //TODO Auto-generated method stub
-    return data;
+	this->status = status;
 }
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-void Ordem_de_Servico::set_data(string data) 
+
+Buraco Ordem_de_Servico::getburaco()
 {
-    //TODO Auto-generated method stub
-    this->data = data;
+	return this->buraco;
 }
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-char Ordem_de_Servico::get_prioridade() 
+
+void Ordem_de_Servico::setburaco(Buraco buraco)
 {
-    //TODO Auto-generated method stub
-    return prioridade;
-}
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-void Ordem_de_Servico::set_prioridade(char prioridade) 
-{
-    //TODO Auto-generated method stub
-    this->prioridade = prioridade;
-}
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-int Ordem_de_Servico::get_estimativaPessoal() 
-{
-    //TODO Auto-generated method stub
-    return estimativaPessoal;
-}
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-void Ordem_de_Servico::set_estimativaPessoal(int estimativaPessoal) 
-{
-    //TODO Auto-generated method stub
-    this->estimativaPessoal = estimativaPessoal;
-}
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-int Ordem_de_Servico::get_estimativaEquipamento() 
-{
-    //TODO Auto-generated method stub
-    return estimativaEquipamento;
-}
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-void Ordem_de_Servico::set_estimativaEquipamento(int estimativaEquipamento) 
-{
-    //TODO Auto-generated method stub
-    this->estimativaEquipamento = estimativaEquipamento;
-}
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-int Ordem_de_Servico::get_estimativaMaterial() 
-{
-    //TODO Auto-generated method stub
-    return estimativaMaterial;
-}
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-void Ordem_de_Servico::set_estimativaMaterial(int estimativaMaterial) 
-{
-    //TODO Auto-generated method stub
-    this->estimativaMaterial = estimativaMaterial;
-}
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-int Ordem_de_Servico::get_estimativaCusto() 
-{
-    //TODO Auto-generated method stub
-    return estimativaCusto;
-}
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-void Ordem_de_Servico::set_estimativaCusto(int estimativaCusto) 
-{
-    //TODO Auto-generated method stub
-    this->estimativaCusto = estimativaCusto;
-}
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-char Ordem_de_Servico::get_status() 
-{
-    //TODO Auto-generated method stub
-    return status;
-}
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-void Ordem_de_Servico::set_status(char status) 
-{
-    //TODO Auto-generated method stub
-    this->status = status;
-}
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-Notificacao * Ordem_de_Servico::get_Notificacao() 
-{
-    //TODO Auto-generated method stub
-    return Notificacao;
-}
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-void Ordem_de_Servico::set_Notificacao(Notificacao * Notificacao) 
-{
-    //TODO Auto-generated method stub
-}
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-Saida * Ordem_de_Servico::get_Saida() 
-{
-    //TODO Auto-generated method stub
-    return Saida;
-}
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-void Ordem_de_Servico::set_Saida(Saida * Saida) 
-{
-    //TODO Auto-generated method stub
-}
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-Encarregado * Ordem_de_Servico::get_encarregado() 
-{
-    //TODO Auto-generated method stub
-    return encarregado;
-}
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-void Ordem_de_Servico::set_encarregado(Encarregado * encarregado) 
-{
-    //TODO Auto-generated method stub
-}
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-void Ordem_de_Servico::obterOSFinalizada() 
-{
-    //TODO Auto-generated method stub
-    return;
+	this->buraco = buraco;
 }
--- Project1/Ordem_de_Servico.h
+++ Project1/Ordem_de_Servico.h
@@ -1,295 +1,45 @@
 #ifndef ORDEM_DE_SERVICO_H
 #define ORDEM_DE_SERVICO_H
-//Begin section for file Ordem_de_Servico.h
-//TODO: Add definitions that you want preserved
-//End section for file Ordem_de_Servico.h
+
 #include "XSDDataTypes/time.h"
 #include "Material.h"
 #include "java/sql/Time.h"
+#include <iostream>
+#include <ctime>
+using namespace std;
 
-
-
-class Notificacao; //Dependency Generated Source:Ordem_de_Servico Target:Notificacao
-
-
-class Saida; //Dependency Generated Source:Ordem_de_Servico Target:Saida
-
-
-class Encarregado; //Dependency Generated Source:Ordem_de_Servico Target:Encarregado
-
-
-
-
-
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
 class Ordem_de_Servico
 {
-
-    //Begin section for Ordem_de_Servico
-    //TODO: Add attributes that you want preserved
-    //End section for Ordem_de_Servico
-
-    private:
-
-
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        int id;
-
-
-
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        time horaPessoal;
-
-
-
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        Material material;
-
-
-
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        Time horaEquipamento;
-
-
-
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        string data;
-
-
-
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        char prioridade;
-
-
-
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        int estimativaPessoal;
-
-
-
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        int estimativaEquipamento;
-
-
-
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        int estimativaMaterial;
-
-
-
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        int estimativaCusto;
-
-
-
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        char status;
-
-
-
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        Notificacao * Notificacao;
-
-
-
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        Saida * Saida;
-
-
-
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        Encarregado * encarregado;
-
-
-
-
-    public:
-
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        Ordem_de_Servico(); 
-
-
-
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        virtual ~Ordem_de_Servico(); 
-
-
-
-        //get id
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline int get_id(); 
-
-
-
-        //set id
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline void set_id(int id); 
-
-
-
-        //get horaPessoal
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline time get_horaPessoal(); 
-
-
-
-        //set horaPessoal
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline void set_horaPessoal(time horaPessoal); 
-
-
-
-        //get material
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline Material get_material(); 
-
-
-
-        //set material
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline void set_material(Material material); 
-
-
-
-        //get horaEquipamento
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline Time get_horaEquipamento(); 
-
-
-
-        //set horaEquipamento
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline void set_horaEquipamento(Time horaEquipamento); 
-
-
-
-        //get data
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline string get_data(); 
-
-
-
-        //set data
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline void set_data(string data); 
-
-
-
-        //get prioridade
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline char get_prioridade(); 
-
-
-
-        //set prioridade
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline void set_prioridade(char prioridade); 
-
-
-
-        //get estimativaPessoal
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline int get_estimativaPessoal(); 
-
-
-
-        //set estimativaPessoal
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline void set_estimativaPessoal(int estimativaPessoal); 
-
-
-
-        //get estimativaEquipamento
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline int get_estimativaEquipamento(); 
-
-
-
-        //set estimativaEquipamento
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline void set_estimativaEquipamento(int estimativaEquipamento); 
-
-
-
-        //get estimativaMaterial
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline int get_estimativaMaterial(); 
-
-
-
-        //set estimativaMaterial
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline void set_estimativaMaterial(int estimativaMaterial); 
-
-
-
-        //get estimativaCusto
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline int get_estimativaCusto(); 
-
-
-
-        //set estimativaCusto
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline void set_estimativaCusto(int estimativaCusto); 
-
-
-
-        //get status
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline char get_status(); 
-
-
-
-        //set status
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline void set_status(char status); 
-
-
-
-        //get Notificacao
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline Notificacao * get_Notificacao(); 
-
-
-
-        //set Notificacao
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline void set_Notificacao(Notificacao * Notificacao); 
-
-
-
-        //get Saida
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline Saida * get_Saida(); 
-
-
-
-        //set Saida
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline void set_Saida(Saida * Saida); 
-
-
-
-        //get encarregado
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline Encarregado * get_encarregado(); 
-
-
-
-        //set encarregado
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        inline void set_encarregado(Encarregado * encarregado); 
-
-
-
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        void obterOSFinalizada(); 
-
-
-
-};  //end class Ordem_de_Servico
-
-
+private:
+	int idordem;
+	int horapessoal;
+	time_t data;
+	int prioridade;
+	string status;
+	Buraco buraco;
+
+public:
+	Ordem_de_Servico();
+	virtual ~Ordem_de_Servico();
+
+	int getidordem();
+	void setidordem(int id);
+
+	int gethorapessoal();
+	void sethorapessoal(int hora);
+
+	time_t getdata();
+	void setdata(time_t data);
+
+	int getprioridade();
+	void setprioridade(int prioridade);
+
+	string getstatus();
+	void setstatus(string status);
+
+	Buraco getburaco();
+	void setburaco(Buraco buraco);
+}
 
 #endif
+
